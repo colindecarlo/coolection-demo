dc576cfd8dbb00ce72b3882213090a55e0bbc28c
Colin DeCarlo <colin@thedecarlos.ca>
2015-03-01 12:20:55 -0500

    Remove GuzzleHttp

--- demo.php
+++ demo.php
@@ -3,40 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use JankProfiler\JankProfiler;
-use Illuminate\Support\Collection;
-use GuzzleHttp\Client as HttpClient;
 
-function get_list_of_admin_regions()
-{
-    $geonames = new HttpClient([
-        'base_url' => 'http://api.geonames.org',
-        'defaults' => [
-            'query' => ['username' => 'coolection']
-        ]
-    ]);
-    
-    $countries = json_decode($geonames->get('/countryInfoJSON')->getBody()->getContents())->geonames;
-
-    $getAdminRegions = function ($country) use ($geonames) {
-        $response = $geonames->get('/childrenJSON', ['query' => ['geonameId' => $country->geonameId]])
-            ->getBody()
-            ->getContents();
-        $regions = json_decode($response);
-
-        return property_exists($regions, 'geonames')
-            ? $regions->geonames
-            : [];
-    };
-
-    $regions = [];
-    foreach($countries as $country) {
-        $regions = array_merge($regions, $getAdminRegions($country));
-    }
-
-    return $regions;
-}
-
-//$regions = get_list_of_admin_regions();
 $regions = json_decode(file_get_contents('./fixtures/admin_regions.json'));
 $cloneArrayContents = function() use ($regions) {
     return array_map(function ($elem) {
