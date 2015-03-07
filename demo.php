82245e2beb6241f007461a75a0385e88114d50ba
Colin DeCarlo <colin@thedecarlos.ca>
2015-03-06 20:43:24 -0500

    Add flatten demo

--- demo.php
+++ demo.php
@@ -5,31 +5,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 use JankProfiler\JankProfiler;
 
 $regions = json_decode(file_get_contents('./fixtures/admin_regions.json'));
-$cloneArrayContents = function() use ($regions) {
+$cloneArrayContents = function($arr) {
     return array_map(function ($elem) {
         return clone $elem;
-    }, $regions);
+    }, $arr);
 };
-$collection = new JankProfiler('Illuminate\\Support\\Collection', $cloneArrayContents());
-$coolection = new JankProfiler('Coolection\\Coolection', $cloneArrayContents());
+$collection = new JankProfiler('Illuminate\\Support\\Collection', $cloneArrayContents($regions));
+$speedBag = new JankProfiler('SpeedBag\\SpeedBag', $cloneArrayContents($regions));
 
-$report1 = $coolection->report('array')[0];
-$report2 = $collection->report('array')[0];
+report('construct', $speedBag, $collection);
 
-var_dump([
-    'coolection' => [
-        'memory_before' => $report1['memory_before'],
-        'memory_after' => $report1['memory_after'],
-        'delta' => $report1['memory_after'] - $report1['memory_before']
-    ],
-    'collection' => [
-        'memory_before' => $report2['memory_before'],
-        'memory_after' => $report2['memory_after'],
-        'delta' => $report2['memory_after'] - $report2['memory_before']
-    ],
-]);
-
-$coolection->map(function ($region) {
+$speedBag->map(function ($region) {
     return strtoupper($region->name);
 });
 
@@ -37,22 +23,45 @@ $collection->map(function ($region) {
     return strtoupper($region->name);
 });
 
-$tmp = $coolection->report('array');
-$report1 = array_pop($tmp);
-$tmp = $collection->report('array');
-$report2 = array_pop($tmp);
+report('map', $speedBag, $collection);
 
-var_dump([
-    'coolection' => [
-        'memory_before' => $report1['memory_before'],
-        'memory_after' => $report1['memory_after'],
-        'memory_delta' => $report1['memory_after'] - $report1['memory_before'],
-        'execution_delta' => $report1['end_time'] - $report1['start_time']
-    ],
-    'collection' => [
-        'memory_before' => $report2['memory_before'],
-        'memory_after' => $report2['memory_after'],
-        'memory_delta' => $report2['memory_after'] - $report2['memory_before'],
-        'execution_delta' => $report2['end_time'] - $report2['start_time']
-    ],
-]);
+$usStates = function ($region) {
+    return property_exists($region, 'countryName') && $region->countryName == 'United States';
+};
+
+$speedBag->filter($usStates);
+$collection->filter($usStates);
+report('filter', $speedBag, $collection);
+
+
+$regionsByCountry = json_decode(file_get_contents('./fixtures/admin_regions_by_country.json'));
+$collection = new JankProfiler('Illuminate\\Support\\Collection', $regionsByCountry);
+$speedBag = new JankProfiler('SpeedBag\\SpeedBag', $regionsByCountry);
+
+$collection->flatten();
+$speedBag->flatten();
+report('flatten', $speedBag, $collection);
+
+function report($action, $speedBag, $collection)
+{
+    $tmp = $speedBag->report('array');
+    $report1 = array_pop($tmp);
+    $tmp = $collection->report('array');
+    $report2 = array_pop($tmp);
+
+    var_dump([
+        'action' => $action,
+        'speedBag' => [
+            'memory_before' => $report1['memory_before'],
+            'memory_after' => $report1['memory_after'],
+            'memory_delta' => $report1['memory_after'] - $report1['memory_before'],
+            'execution_delta' => $report1['end_time'] - $report1['start_time']
+        ],
+        'collection' => [
+            'memory_before' => $report2['memory_before'],
+            'memory_after' => $report2['memory_after'],
+            'memory_delta' => $report2['memory_after'] - $report2['memory_before'],
+            'execution_delta' => $report2['end_time'] - $report2['start_time']
+        ],
+    ]);
+}
